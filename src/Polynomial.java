06dce16886cb6a0dd3a3fa27f587c83a53a00faf
RebekahTippy <rebekahtippy@gmail.com>
2022-02-13 19:02:09 -0600

    no 0 coef terms are added polys are populated with no terms that can be added dropTerm method was added to poly class

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -2,10 +2,16 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Polynomial<Term> extends ArrayList<Term> {
+
+public class Polynomial<T> extends ArrayList<Term> {
 
 	//Constructors
 	public Polynomial() {}
 	
 	//needs a sort method and an add method
+	
+	public void dropTerm(Term t) { 
+		this.remove(t);
+	}
+	
 	}
--- src/Polynomial_Calculator.java
+++ src/Polynomial_Calculator.java
@@ -1,5 +1,7 @@
 
 import java.util.Scanner;
+
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -40,9 +42,35 @@ public class Polynomial_Calculator {
 		System.out.println("Enter first polynomial with no spaces. Use '^' to represent powers. (example: 3x^3-x^2+1): ");//asks user for first polynomial to add
 		String userInput = input.nextLine();
 		
-		int userInputIndex = 0;
-		while (userInputIndex < userInput.length()) {//Iterates through the users polynomial extracting the Terms
-			userInputIndex = extractTerm(1, userInput, userInputIndex);// change first param to 2 when asking for second poly
+		int returnPack[] = new int[4];
+		returnPack = extractTerm(userInput, 0);//Contents 0: coefficient, 1: variable (0 = ' ', 1 = 'x'), 2:exponent, 3: index To Start At next
+		
+		while (returnPack[3] <= userInput.length()) {//Iterates through the users polynomial extracting the Terms and adding them to firstPoly. 
+			                                         //also combines terms with the same coefficient and wont add terms with 0 a coefficient.
+			int Coef = returnPack[0];
+			int varNum = returnPack[1];
+			int Expo = returnPack[2];//Makes things more readable
+			char var = ' ';
+			if (varNum == 1) {var = 'x';}//converts var number to letter for Term constructor
+			
+			if (Coef != 0) {//only add if coefficient is not 0
+				if (firstPoly.size()==0) {//if there are no other terms add this one
+					Term newTerm = new Term(Coef, var, Expo);
+					firstPoly.add(newTerm);
+				}else {//if there are other terms check to see if you can add with a term with the same exponent
+					for (int d=0; d < firstPoly.size(); d++) {
+						if (firstPoly.get(d).getExponent() == Expo && firstPoly.get(d).getVariable()== var) {//A Term matches the variable and exponent of the term to be added
+								Coef = Coef + firstPoly.get(d).getCoefficient();//adds the term that matches coefficient to the new terms coefficient
+								firstPoly.dropTerm(firstPoly.get(d));//deletes the term that matches
+							}
+						}
+						if (Coef != 0) {//Confirms that the new Coef is still not 0 and adds the term
+							Term newTerm = new Term(Coef, var, Expo);
+							firstPoly.add(newTerm);
+						}
+				}
+			}
+			returnPack = extractTerm(userInput, returnPack[3]);// Updates returnPack with the data for the next Term
 		}
 				
 		
@@ -60,73 +88,76 @@ public class Polynomial_Calculator {
 	}//end main
 	
 		
-/**
- * Extracts the variable, exponent, and coefficient from the users string and adds them to a new term.
- * That term is added to a polynomial based on the polyIDNumber. 
- * returns the index to start extracting the next term at.
- * @param polyIDNumber: Identifies witch polynomial to add term to
- * @param poly: Original string that user entered
- * @param start: the index to start parsing the string
- * @return: the index to start parsing the string next time
- */
-public static int extractTerm(int polyIDNumber, String poly, int start) {
-	int polyID = polyIDNumber;
-	char variable = ' ';
-	StringBuilder coef = new StringBuilder();
-	StringBuilder expo = new StringBuilder();
-	int i = start;
-	poly = poly + " ";//the extra space is added to avoid out of bounds index error.
-	
+	/**
+	 * Extracts the variable, exponent, and coefficient from the users string and
+	 * adds them to an array with the index to start extracting the next term at.
+	 * @param poly: Original string that user entered
+	 * @param start: the index to start parsing the string
+	 * @return: pack[]: an array of ints containing elements in this order: coefficient, variable, exponent, index to start at next
+	 */
+	public static int[] extractTerm(String polLen, int start) {
 
+		
+		int[] pack = new int[4];
+		int variable = 0;//if it is 0 there is no x term. if it is 1 there is
+		StringBuilder coef = new StringBuilder();
+		StringBuilder expo = new StringBuilder();
+		int i = start;
+		String poly = polLen;
+		poly = poly + " ";//the extra space is added to avoid out of bounds index error.
 	
-	if (poly.charAt(i) == '-') {//first character is a - sign
-		coef.append('-');
-		i++;
+		if (start >= polLen.length()) {
+			pack[3]=polLen.length()+1;
+			return pack;
+		}
 	
-	} else if (poly.charAt(i)== '+') {//first character is a + sign
-		i++;
-	}
+		if (poly.charAt(i) == '−' || poly.charAt(i) == '-') {//first character is a - sign
+			coef.append('-');
+			i++;
+	
+		} else if (poly.charAt(i)== '+') {//first character is a + sign
+			i++;
+	
+		}
  
-	if (Character.isDigit(poly.charAt(i))) {//coefficient is greater than 1
-		while (Character.isDigit(poly.charAt(i))) {
-			coef.append(poly.charAt(i));
+		if (Character.isDigit(poly.charAt(i))) {//coefficient is greater than 1
+			while (Character.isDigit(poly.charAt(i))) {
+				coef.append(poly.charAt(i));
+				i++;
+			}
+			if (poly.charAt(i) == 'x') {//an x follows the coefficient number
+				variable = 1;
+				i++;
+			}
+ 
+		} else if (poly.charAt(i) == 'x') {//first term after sign is x
+			variable = 1;
+			coef.append('1');
 			i++;
 		}
-		if (poly.charAt(i) == 'x') {//an x follows the coefficient number
-			variable = 'x';
-			i++;
-		}
  
-	} else if (poly.charAt(i) == 'x') {//first term after sign is x
-	 		variable = 'x';
-	 		coef.append('1');
-	 		i++;
-		}
- 
-	if (poly.charAt(i)== '^'){//the exponent is greater than 1
-		++i;
-		if (poly.charAt(i) == '-') {//a - sign follows the ^ sign
-			expo.append('-');
-			i++;
-		}
+		if (poly.charAt(i)== '^'){//the exponent is greater than 1
+			++i;
 	
-		while (Character.isDigit(poly.charAt(i))) {//records exponent number
-			 expo.append(poly.charAt(i));
-			 i++; 
-		} 	 
-	} else {//there is no ^ sign
-		expo.append('1');
-	}
+			if (poly.charAt(i) == '−' || poly.charAt(i) == '-') {//a - sign follows the ^ sign
+				expo.append('-');
+				i++;
+			}
+	
+			while (Character.isDigit(poly.charAt(i))) {//records exponent number
+				expo.append(poly.charAt(i));
+				i++; 
+			} 	 
+		} else {//there is no ^ sign
+			expo.append('1');
+		}
 
-	int FinalCoef = Integer.parseInt(coef.toString());//converts the string builders into integers
-	int FinalExpo =Integer.parseInt(expo.toString());
+		pack[0] = Integer.parseInt(coef.toString());//converts the coef from a StringBuilder to an int
+		pack[1] = variable;
+		pack[2] = Integer.parseInt(expo.toString());
+		pack[3] = i;
  
-	Term unit = new Term(FinalCoef, variable ,FinalExpo);//creates a term with the coefficient, variable and exponent
- 
-	if (polyID == 1) {firstPoly.add(unit);}//selects what polynomial to add the term based on polyID
-	else if (polyID == 2) {secondPoly.add(unit);}
-
-	return i;
+		return pack;
 	}//end extractTerm
 
 }//end class
