c713867aded57afcf6802ef286e285b5ec79c7d1
RebekahTippy <rebekahtippy@gmail.com>
2022-02-15 15:37:03 -0600

    default menu case added white space fixed poly index issue fixed

--- src/Polynomial_Calculator.java
+++ src/Polynomial_Calculator.java
@@ -22,81 +22,85 @@ public class Polynomial_Calculator {
 				   + "q: Quit"
 				   + "\n"
 				   + "Please type \"f\" \"s\" \"a\" \"p\" or \"q\":");
-char userChoice = input.nextLine().charAt(0);
-System.out.println();
+		char userChoice = input.nextLine().charAt(0);
+		System.out.println();
 
-while (userChoice != 'q'){
-	switch(userChoice) {
-	
-		case 'f'://enters the first polynomial
-			System.out.println("Enter polynomial with no spaces. Use '^' to represent powers.\n ( example: \"2x^3-3x^-2+3+x+3x\" ): ");//asks user for first polynomial to add
-			String userInput = input.nextLine();
-			
-			if (firstPoly.size()!= 0) {//clears the first poly so you can fill it with new terms
-				firstPoly.clear();
-			}
-			
-			
-			returnPack = extractTerm(userInput, 0);//Contents 0: coefficient, 1: variable (0 = ' ', 1 = 'x'), 2:exponent, 3: index To Start At next
-			
-			while (returnPack[3] <= userInput.length()) {//Iterates polynomial extracting Terms adding them to firstPoly. combines terms with same coefficient and wont add terms with 0 coefficient.
-				variable = ' ';
-				if (returnPack[1] == 1) {variable = 'x';}//converts variable number to letter for Term constructor
-				Term newTerm = new Term(returnPack[0], variable, returnPack[2]);
-				firstPoly.addTerm(newTerm);
-				
-				returnPack = extractTerm(userInput, returnPack[3]);// Updates returnPack with the data for the next Term
-			}
+		while (userChoice != 'q'){
+			switch(userChoice) {
+				case 'f'://enters the first polynomial
+					System.out.println("Enter polynomial with no spaces. Use '^' to represent powers.\n ( example: \"2x^3-3x^-2+3+x+3x\" ): ");//asks user for first polynomial to add
+					String userInput = input.nextLine();
+					if (firstPoly.size()!= 0) {//clears the first poly so you can fill it with new terms
+						firstPoly.clear();
+					}
+					returnPack = extractTerm(userInput, 0);//Contents 0: coefficient, 1: variable (0 = ' ', 1 = 'x'), 2:exponent, 3: index To Start At next
+					while (returnPack[3] <= userInput.length()) {//Iterates polynomial extracting Terms adding them to firstPoly. combines terms with same coefficient and wont add terms with 0 coefficient.
+						variable = ' ';
+						if (returnPack[1] == 1) {
+							variable = 'x';
+						}//converts variable number to letter for Term constructor
+						Term newTerm = new Term(returnPack[0], variable, returnPack[2]);
+						firstPoly.addTerm(newTerm);
+						returnPack = extractTerm(userInput, returnPack[3]);// Updates returnPack with the data for the next Term
+					}//end while
 					firstPoly.sortPoly();
-			break;
+					break;//end 'f' case
 			
-		case 's':
-			System.out.println("Enter polynomial with no spaces. Use '^' to represent powers.\n ( example: \"2x^3-3x^-2+3+x+3x\" ): ");//asks user for first polynomial to add
-			userInput = input.nextLine();
+					
+				case 's':
+					System.out.println("Enter polynomial with no spaces. Use '^' to represent powers.\n ( example: \"2x^3-3x^-2+3+x+3x\" ): ");//asks user for first polynomial to add
+					userInput = input.nextLine();
+					if (secondPoly.size()!= 0) {//clears the second poly so you can fill it with new terms
+						secondPoly.clear();
+					}
+					returnPack = extractTerm(userInput, 0);//Contents 0: coefficient, 1: variable (0 = ' ', 1 = 'x'), 2:exponent, 3: index To Start At next
 			
-			if (secondPoly.size()!= 0) {//clears the second poly so you can fill it with new terms
-				secondPoly.clear();
-			}
-			
-			returnPack = extractTerm(userInput, 0);//Contents 0: coefficient, 1: variable (0 = ' ', 1 = 'x'), 2:exponent, 3: index To Start At next
-			
-			while (returnPack[3] <= userInput.length()) {//Iterates polynomial extracting Terms adding them to firstPoly. combines terms with same coefficient and wont add terms with 0 coefficient.
-				variable = ' ';
-				if (returnPack[1] == 1) {variable = 'x';}//converts variable number to letter for Term constructor
-				Term newTerm = new Term(returnPack[0], variable, returnPack[2]);
-				secondPoly.addTerm(newTerm);
-				
-				returnPack = extractTerm(userInput, returnPack[3]);// Updates returnPack with the data for the next Term
-			}
+					while (returnPack[3] <= userInput.length()) {//Iterates polynomial extracting Terms adding them to firstPoly. combines terms with same coefficient and wont add terms with 0 coefficient.
+						variable = ' ';
+						if (returnPack[1] == 1) {
+							variable = 'x';
+						}//converts variable number to letter for Term constructor
+						Term newTerm = new Term(returnPack[0], variable, returnPack[2]);
+						secondPoly.addTerm(newTerm);
+						returnPack = extractTerm(userInput, returnPack[3]);// Updates returnPack with the data for the next Term
+					}
 					secondPoly.sortPoly();
-			break;
+					break;//end 's' case
 			
-		case 'a'://add the polynomials together
-			if (addedPoly.size() !=0) {//clears old addedPoly data 
-				addedPoly.clear();
-			}
-			addedPoly.combinePoly(firstPoly, secondPoly);//addedPoly = firstPoly + secondPoly
-			addedPoly.sortPoly();
-			System.out.println("\nYour polynomials have been added. \n");
-			break;
+					
+				case 'a'://add the polynomials together
+					if (addedPoly.size() !=0) {//clears old addedPoly data 
+						addedPoly.clear();
+					}
+					addedPoly.combinePoly(firstPoly, secondPoly);//addedPoly = firstPoly + secondPoly
+					addedPoly.sortPoly();
+					System.out.println("\nYour polynomials have been added. \n");
+					break;//end 'a' case
+					
 	
-		case 'p'://print the terms in all the polynomials
-			System.out.println("results: " + firstPoly.toString()+ " + " + secondPoly.toString() + " = " + addedPoly.toString());
+				case 'p'://print the terms in all the polynomials
+					System.out.println("results: " + firstPoly.toString()+ " + " + secondPoly.toString() + " = " + addedPoly.toString());
+					System.out.println();
+					break;//end 'p' case
+					
+					
+				default:
+					System.out.println("The character you entered is unsupported.");
+					break;
+			}//end switch
+			System.out.print("f: Enter first polynomial\n"//prints the menu again after each command is run unless 'q' is entered
+					+ "s: Enter second polynomial\n"
+					+ "a: Add the polynomials\n"
+					+ "p: Print the added polynomial\n"
+					+ "q: Quit"
+					+ "\n"
+					+ "Please type \"f\" \"s\" \"a\" \"p\" or \"q\":");
+			userChoice = input.nextLine().charAt(0);
 			System.out.println();
-			break;
-	}//end switch
-	System.out.print("f: Enter first polynomial\n"//prints the menu again after each command is run unless 'q' is entered
-		   + "s: Enter second polynomial\n"
-		   + "a: Add the polynomials\n"
-		   + "p: Print the added polynomial\n"
-		   + "q: Quit"
-		   + "\n"
-		   + "Please type \"f\" \"s\" \"a\" \"p\" or \"q\":");
-	userChoice = input.nextLine().charAt(0);
-	System.out.println();
-}//end 'q' while
+		}//end 'q' while
 		
 	}//end main
+	
 	
 	/**
 	 * Extracts the variable, exponent, and coefficient from the users string and
@@ -105,22 +109,16 @@ while (userChoice != 'q'){
 	 * @param start: the index to start parsing the string
 	 * @return: pack[]: an array of ints containing elements in this order: coefficient, variable, exponent, index to start at next
 	 */
-	public static int[] extractTerm(String polLen, int start) {
-
-		
+	public static int[] extractTerm(String poly, int start) {
 		int[] pack = new int[4];
 		int variable = 0;//if it is 0 there is no x term. if it is 1 there is
 		StringBuilder coef = new StringBuilder();
 		StringBuilder expo = new StringBuilder();
 		int i = start;
-		String poly = polLen;
-		poly = poly + " ";//the extra space is added to avoid out of bounds index error.
-	
-		if (start >= polLen.length()) {
-			pack[3]=polLen.length()+1;
+		
+		if (start >= poly.length()) {
 			return pack;
 		}
-	
 		if (poly.charAt(i) == '−' || poly.charAt(i) == '-') {//first character is a - sign
 			coef.append('-');
 			i++;
@@ -131,22 +129,22 @@ while (userChoice != 'q'){
 		}
  
 		if (Character.isDigit(poly.charAt(i))) {//coefficient is greater than 1
-			while (Character.isDigit(poly.charAt(i))) {
+			while (i < poly.length() && Character.isDigit(poly.charAt(i))) {
 				coef.append(poly.charAt(i));
 				i++;
 			}
-			if (poly.charAt(i) == 'x') {//an x follows the coefficient number
+			if (i < poly.length() && poly.charAt(i) == 'x') {//an x follows the coefficient number
 				variable = 1;
 				i++;
 			}
  
-		} else if (poly.charAt(i) == 'x') {//first term after sign is x
+		} else if (i < poly.length() && poly.charAt(i) == 'x') {//first term after sign is x
 			variable = 1;
 			coef.append('1');
 			i++;
 		}
  
-		if (poly.charAt(i)== '^'){//the exponent is greater than 1
+		if (i < poly.length() && poly.charAt(i)== '^'){//the exponent is greater than 1
 			++i;
 	
 			if (poly.charAt(i) == '−' || poly.charAt(i) == '-') {//a - sign follows the ^ sign
@@ -154,7 +152,7 @@ while (userChoice != 'q'){
 				i++;
 			}
 	
-			while (Character.isDigit(poly.charAt(i))) {//records exponent number
+			while (i < poly.length() && Character.isDigit(poly.charAt(i))) {//records exponent number
 				expo.append(poly.charAt(i));
 				i++; 
 			} 	 
