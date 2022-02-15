e22ee3889e6c155b172055786878687a8ec87941
RebekahTippy <rebekahtippy@gmail.com>
2022-02-14 18:01:53 -0600

    import error fixed

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ListIterator;
 
-import polynomial_addition.Term;
 
 
 
--- src/Polynomial_Calculator.java
+++ src/Polynomial_Calculator.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
 
-import polynomial_addition.Term;
 
 import java.util.ArrayList;
 import java.util.Iterator;
